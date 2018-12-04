07123a569c77bb3f860f687b1cd8a572fce8134b
voii83 <voiivoii83@gmail.com>
2018-12-04 23:20:42 +0300

    new grid without bootstrap, media-query, header is ready without a mobile version

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,4 @@
 
 //= ../../../node_modules/jquery/dist/jquery.js
-//= ../../../node_modules/popper.js/dist/umd/popper.js
-//= ../../../node_modules/bootstrap/js/dist/util.js
-
 //= ../../../node_modules/slick-carousel/slick/slick.js
-
 //= custom/main-slick.js
